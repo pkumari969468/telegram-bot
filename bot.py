0f223ac9f2a7f0a4f58e77afd049b25b5ce11470
pkumari969468 <pkumari969468@gmail.com>
2026-05-28 15:51:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,17 @@
 from flask import Flask, request
 import telebot
+from pymongo import MongoClient
 import os
 
 TOKEN = "8702770051:AAGt1WgLNqYKprabfI7hEDA14qzQnThvpQQ"
+
+MONGO_URL = "YOUR_MONGODB_URL"
+
+client = MongoClient(MONGO_URL)
+
+db = client["telegram_bot"]
+
+users_collection = db["users"]
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -11,7 +20,22 @@ app = Flask(__name__)
 @bot.message_handler(commands=['start'])
 def start(message):
 
-    bot.reply_to(message, "Bot Working ✅")
+    user_id = message.chat.id
+
+    existing_user = users_collection.find_one(
+        {"user_id": user_id}
+    )
+
+    if not existing_user:
+
+        users_collection.insert_one(
+            {"user_id": user_id}
+        )
+
+    bot.reply_to(
+        message,
+        "Bot + MongoDB Working ✅"
+    )
 
 @app.route(f"/{TOKEN}", methods=['POST'])
 def webhook():
